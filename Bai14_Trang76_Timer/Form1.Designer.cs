0be820e9cb6f4b6d58961310a1eb8a268474f137
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-06 23:38:30 +0700

    DungNguyen

---
+++ Bai14_Trang76_Timer/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Bai14_Trang76_Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblVTH = new System.Windows.Forms.Label();
+            this.tmrHenGio = new System.Windows.Forms.Timer(this.components);
+            this.lblTraiSangPhai = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblVTH
+            // 
+            this.lblVTH.AutoSize = true;
+            this.lblVTH.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVTH.ForeColor = System.Drawing.Color.Red;
+            this.lblVTH.Location = new System.Drawing.Point(41, 38);
+            this.lblVTH.Name = "lblVTH";
+            this.lblVTH.Size = new System.Drawing.Size(218, 39);
+            this.lblVTH.TabIndex = 0;
+            this.lblVTH.Text = "Vũ Thu Hiền";
+            this.lblVTH.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tmrHenGio
+            // 
+            this.tmrHenGio.Tick += new System.EventHandler(this.tmrHenGio_Tick);
+            // 
+            // lblTraiSangPhai
+            // 
+            this.lblTraiSangPhai.AutoSize = true;
+            this.lblTraiSangPhai.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTraiSangPhai.ForeColor = System.Drawing.Color.Red;
+            this.lblTraiSangPhai.Location = new System.Drawing.Point(41, 119);
+            this.lblTraiSangPhai.Name = "lblTraiSangPhai";
+            this.lblTraiSangPhai.Size = new System.Drawing.Size(218, 39);
+            this.lblTraiSangPhai.TabIndex = 1;
+            this.lblTraiSangPhai.Text = "Vũ Thu Hiền";
+            this.lblTraiSangPhai.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(1153, 206);
+            this.Controls.Add(this.lblTraiSangPhai);
+            this.Controls.Add(this.lblVTH);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Điều khiển Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblVTH;
+        private System.Windows.Forms.Timer tmrHenGio;
+        private System.Windows.Forms.Label lblTraiSangPhai;
+    }
+}
+
---
+++ Bai14_Trang76_Timer/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai14_Trang76_Timer
+{
+    public partial class Form1 : Form
+    {
+        bool traiSangPhai; // traiSangPhai=true chuyển động sang phải, traiSangPhai=false chuyển động sang trái.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            traiSangPhai = true;
+            tmrHenGio.Interval = 80;  // Chu kì gọi sự kiện tich bên dưới---Đơn vị: ms    (1s = 1000ms)
+                                    // Mặc định là: 100ms
+            tmrHenGio.Start();  // Để bắt đầu       Dừng lại: tmrHenGio.Stop();
+        }
+
+        private void tmrHenGio_Tick(object sender, EventArgs e)
+        {
+            // Tạo chuyển động trái -> phải và phải -> trái
+            if (traiSangPhai)
+            {
+                if ((lblVTH.Left + lblVTH.Width) < this.Width)
+                    lblVTH.Left = lblVTH.Left + 10;
+                else
+                    traiSangPhai = false;
+            }
+            else
+            {
+                if (lblVTH.Left > 0)
+                    lblVTH.Left = lblVTH.Left - 10;
+                else
+                    traiSangPhai = true;
+            }
+
+            // Tạo chuyển động chỉ trái sang phải
+            if ((lblTraiSangPhai.Left + lblTraiSangPhai.Width) < this.Width)
+                lblTraiSangPhai.Left = lblTraiSangPhai.Left + 10;
+            else
+                lblTraiSangPhai.Left = 0;   // Đặt lại vị trí về bên trái
+        }
+    }
+}
